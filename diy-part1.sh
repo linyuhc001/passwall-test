230b346dc9374a10864decc15fb0b5857a937f8e
linyuhc001 <linyuhc001@gmail.com>
2020-12-03 08:37:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -36,6 +36,7 @@ git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-ar
 # svn co https://github.com/db-one/dbone-update/branches/18.06/passwall package/passwall
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
 svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy package/passwall/https-dns-proxy
+rm -rf feeds/packages/net/haproxy
 svn co https://github.com/Lienol/openwrt-packages/trunk/net/haproxy package/passwall/haproxy
 
 # 自定义定制选项
