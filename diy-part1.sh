19b229ae5ea340e02c0b9a970b230776a9fc8a3a
linyuhc001 <linyuhc001@gmail.com>
2020-12-11 20:14:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -44,12 +44,12 @@ sed -i 's#192.168.1.1#192.168.3.105#g' package/base-files/files/bin/config_gener
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings #取消系统默认密码
 sed -i 's#0 6#0 2#g' package/lean/luci-app-adbyby-plus/root/etc/init.d/adbyby #修改adbyby自动更新时间到凌晨2点
 sed -i 's#url-test#fallback#g' package/luci-app-openclash/root/usr/share/openclash/yml_proxys_set.sh #修改openclash自动生成配置中的urltest为fallback
-sed -i 's#net.ipv4.ip_forward=1#net.ipv4.ip_forward=0#g' package/base-files/files/etc/sysctl.d/10-default.conf #修改ipv4forward=0以适配xray的splice，主路由不适用
-sed -i 's#forwarding=1#forwarding=0#g' package/base-files/files/etc/sysctl.d/10-default.conf #修改ipv6forwarding=0以适配xray的splice，主路由不适用
 # sed -i 's#option commit_interval 24h#option commit_interval 10m#g' feeds/packages/net/nlbwmon/files/nlbwmon.config #修改流量统计写入为10分钟
 # sed -i 's#option database_directory /var/lib/nlbwmon#option database_directory /etc/config/nlbwmon_data#g' feeds/packages/net/nlbwmon/files/nlbwmon.config #修改流量统计数据存放默认位置
 # sed -i 's@background-color: #e5effd@background-color: #f8fbfe@g' package/luci-theme-edge/htdocs/luci-static/edge/cascade.css #luci-theme-edge主题颜色微调
 # sed -i 's#rgba(223, 56, 18, 0.04)#rgba(223, 56, 18, 0.02)#g' package/luci-theme-edge/htdocs/luci-static/edge/cascade.css #luci-theme-edge主题颜色微调
+# sed -i 's#net.ipv4.ip_forward=1#net.ipv4.ip_forward=0#g' package/base-files/files/etc/sysctl.d/10-default.conf #修改ipv4forward=0以适配xray的splice，主路由不适用
+# sed -i 's#forwarding=1#forwarding=0#g' package/base-files/files/etc/sysctl.d/10-default.conf #修改ipv6forwarding=0以适配xray的splice，主路由不适用
 
 #创建自定义配置文件 - OpenWrt-x86-64
 
